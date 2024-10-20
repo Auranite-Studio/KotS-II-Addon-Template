f126bd32d0a9281e46fc29bc101906093030f25b
MagicalAlexey <69478092+MagicalAlexey@users.noreply.github.com>
2024-10-20 23:45:57 +0300

    Update to CPAPIv18

--- src/main/java/com/esmods/keepersofthestonesaddontemplate/potion/CustomMasterMobEffect.java
+++ src/main/java/com/esmods/keepersofthestonesaddontemplate/potion/CustomMasterMobEffect.java
@@ -5,7 +5,6 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffect;
 
-import com.esmods.keepersofthestonesaddontemplate.procedures.EnhancedAbilitiesProcedure;
 import com.esmods.keepersofthestonesaddontemplate.procedures.CustomMasterStartProcedure;
 
 public class CustomMasterMobEffect extends MobEffect {
@@ -16,16 +15,5 @@ public class CustomMasterMobEffect extends MobEffect {
 	@Override
 	public void onEffectStarted(LivingEntity entity, int amplifier) {
 		CustomMasterStartProcedure.execute(entity.level(), entity);
-	}
-
-	@Override
-	public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
-		return true;
-	}
-
-	@Override
-	public boolean applyEffectTick(LivingEntity entity, int amplifier) {
-		EnhancedAbilitiesProcedure.execute(entity);
-		return super.applyEffectTick(entity, amplifier);
 	}
 }
